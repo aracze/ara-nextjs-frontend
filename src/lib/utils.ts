961e2e9b8845ef107d6434daf9572373231476ac
Lam Tran <lojzatran@gmail.com>
2026-01-23 15:27:57 +0100

    Fix build

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,6 +5,10 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
+export function isProduction() {
+  return process.env.NODE_ENV === "production";
+}
+
 export function getStrapiURL() {
   return (process.env.STRAPI_BASE_API_URL || "http://localhost:1337").replace(
     /\/$/,
