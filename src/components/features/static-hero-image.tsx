7270d57b7f5106c2b4609dc64016b491b7e237e9
Lam Tran <lojzatran@gmail.com>
2026-04-11 13:17:30 +0200

    Fix warning on nextjs

--- src/components/features/static-hero-image.tsx
+++ src/components/features/static-hero-image.tsx
@@ -12,7 +12,9 @@ export const StaticHeroImage = ({
       className="absolute inset-0 bg-cover bg-no-repeat bg-center transition-transform duration-[10000ms] hover:scale-105"
       style={{
         backgroundImage: imageUrl ? `url(${imageUrl})` : "none",
-        backgroundPosition: styleCss || "50% 35%",
+        backgroundPosition: styleCss
+          ? styleCss.replace(/background-position:\s*/i, "").replace(/;/g, "").trim()
+          : "50% 35%",
       }}
     />
   );
